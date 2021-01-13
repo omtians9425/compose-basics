af7af5702797fa1feb76f1649b81147eade68f1e
omtians9425 <rskowgshakhtardonetsk32517@gmail.com>
2021-01-14 00:15:03 +0900

    Use Surface for adding background color

--- app/src/main/java/com/example/compose/basics/MainActivity.kt
+++ app/src/main/java/com/example/compose/basics/MainActivity.kt
@@ -2,12 +2,16 @@ package com.example.compose.basics
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.setContent
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.compose.basics.ui.theme.ComposebasicsTheme
 
 class MainActivity : AppCompatActivity() {
@@ -26,7 +30,9 @@ class MainActivity : AppCompatActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Surface(color = Color.Yellow) {
+        Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
+    }
 }
 
 @Preview(showBackground = true)

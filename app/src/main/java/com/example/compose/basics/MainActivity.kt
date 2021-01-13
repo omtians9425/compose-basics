90893fef00e2bee59b2c05216f81055ed66d9f6d
omtians9425 <rskowgshakhtardonetsk32517@gmail.com>
2021-01-14 00:35:26 +0900

    Introduce Counter with mutableState

--- app/src/main/java/com/example/compose/basics/MainActivity.kt
+++ app/src/main/java/com/example/compose/basics/MainActivity.kt
@@ -4,10 +4,13 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
 import androidx.compose.material.Divider
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.setContent
@@ -42,6 +45,8 @@ fun MyScreenContent(names: List<String> = listOf("Android", "there")) {
             Greeting(it)
             Divider(color = Color.Black)
         }
+        Divider(color = Color.Transparent, thickness = 32.dp)
+        Counter()
     }
 }
 
@@ -49,6 +54,15 @@ fun MyScreenContent(names: List<String> = listOf("Android", "there")) {
 fun Greeting(name: String) {
     Surface(color = Color.Yellow) {
         Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
+    }
+}
+
+@Composable
+fun Counter() {
+    val count = remember { mutableStateOf(0) }
+
+    Button(onClick = { count.value++ }) {
+        Text(text = "I've been clicked ${count.value} times")
     }
 }
 
